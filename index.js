ac9d170f53ada4d20748d5afd5eaaad9f803446c
Dasha Savostina <moskvichka123@yandex.ru>
2023-01-22 10:44:56 +0300

    ПР4

---
+++ index.js
@@ -0,0 +1,30 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_job');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+function showPopup() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', showPopup);
+closeButton.addEventListener('click', closePopup);
+
+function handleFormSubmit (evt) {
+  evt.preventDefault(); 
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  closePopup();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
